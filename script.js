a7e3a354c8f919cb0979b3ebe8295645e5ded02d
EricIsMyHero <ericismyhero2467@gmail.com>
2025-08-19 00:32:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ fetch("links.json")
     });
   })
   .catch(error => console.error("Linkləri yükləmək mümkün olmadı:", error));
+
+
+        function shareSite() {
+            const shareData = {
+                title: "Səni Sevməyimin 200 Səbəbi 💖",
+                text: "Bax bu sənə həsr olunub 🥺💘",
+                url: window.location.href
+            };
+            if (navigator.share) {
+                navigator.share(shareData).catch(err => console.error("Paylaşmaq mümkün olmadı:", err));
+            } else {
+                alert("Paylaşma funksiyası bu cihazda dəstəklənmir.");
+            }
+        }
